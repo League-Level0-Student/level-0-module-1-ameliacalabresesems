681baea0066129956b291851b16e5f39c0a432d2
calabrese.amelia <calabrese.amelia@SEMS-259-36.SEMS259.LAB>
2019-12-04 14:55:59 -0800

    still in proggress

---
+++ src/_02_unbirthday/HappyUnbirthdayToYou.java
@@ -0,0 +1,27 @@
+package _02_unbirthday;
+
+import javax.swing.JOptionPane;
+
+public class HappyUnbirthdayToYou {
+	public static void main(String[] args) {
+		String cat=JOptionPane.showInputDialog(null, "When is your birthay?");
+if(cat.equals("12/4")) {
+	JOptionPane.showMessageDialog(null, "Happy Birthday");
+} else {
+	JOptionPane.showMessageDialog(null, "Have a very merry unbirthday");
+	
+}
+	
+	
+	
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+}
--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -8,9 +8,11 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-
+String jesse = "You like memes"; 
+String dylan = "you are wierd";
+String lukas = "you are ok at drawing(I'm better though)";
 		// 2. Ask the user to enter a name. Store their answer in a variable.
-
+JOptionPane.showInputDialog(null, "")
 		// 3. In a pop-up, tell the user what is remarkable about that person. 
 
 	}
