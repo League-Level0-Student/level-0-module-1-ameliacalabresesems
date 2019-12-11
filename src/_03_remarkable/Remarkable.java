e4bef5c738f827c554c74c4be64dcc6002ebbe65
calabrese.amelia <calabrese.amelia@SEMS-259-36.SEMS259.LAB>
2019-12-11 14:57:53 -0800

    almost done

--- src/_03_remarkable/Remarkable.java
+++ src/_03_remarkable/Remarkable.java
@@ -8,13 +8,25 @@ public class Remarkable {
 public static void main(String[] args) {
 
 		// 1. Save something remarkable about each person in a variable.
-String jesse = "You like memes"; 
-String dylan = "you are wierd";
+String jesse="You like memes. You prefer Spongebob memes. And Caliou. and whatever memes. You also are bad at spelling memes. You are also always constipated";
+String dylan= "you are weird";
 String lukas = "you are ok at drawing(I'm better though)";
-		// 2. Ask the user to enter a name. Store their answer in a variable.
-JOptionPane.showInputDialog(null, "")
-		// 3. In a pop-up, tell the user what is remarkable about that person. 
 
-	}
+	// 2. Ask the user to enter a name. Store their answer in a variable.
+String input= JOptionPane.showInputDialog(null, "What is your name?"
+		+ "(all lower case)");
+		// 3. In a pop-up, tell the user what is remarkable about that person. 
+if(input.equals("jesse")) {
+	JOptionPane.showMessageDialog(null, jesse);
 }
+if(input.equals("dylan")) {
+JOptionPane.showMessageDialog(null, dylan);
+}
+if(input.equals("lukas")) {
+JOptionPane.showMessageDialog(null, lukas);
+}
+
+}
+	}
+
 
