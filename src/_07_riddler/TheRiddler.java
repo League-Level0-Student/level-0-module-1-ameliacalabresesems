b0a76143eb87014237b3d78e8a751f732e8cef5a
calabrese.amelia <calabrese.amelia@SEMS-259-36.SEMS259.LAB>
2020-01-29 14:57:51 -0800

    zombie eyes

--- src/_07_riddler/TheRiddler.java
+++ src/_07_riddler/TheRiddler.java
@@ -2,7 +2,7 @@ package _07_riddler;
 //    Copyright (c) The League of Amazing Programmers 2013-2019
 //    Level 0
 
-
+import javax.swing.JOptionPane;
 
 public class TheRiddler {
 
@@ -10,16 +10,31 @@ public class TheRiddler {
 
 		// 1. Make a variable to hold the score
 // score
+		int score=0;
 		// 3. Ask the user a riddle. Here are some ideas: bit.ly/some-riddles
 
+String answer=JOptionPane.showInputDialog("Stabbed and tied, have never died. Old as old can be, Push me to the moral ends I'm all for modesty. Often lost and all alone Find me without teeth, The holes I wear. I bear with pride");
 		// 4. If they got the answer right, pop up "correct!" and increase the score by one
-
+if (answer.contentEquals("you are a button")) {
+	JOptionPane.showMessageDialog(null, "correct");
+	score++;
+}
 		// 5. Otherwise, say "wrong" and tell them the answer
-
+else {
+	JOptionPane.showMessageDialog(null, "wrong");	
+}
 		// 6. Add some more riddles
+String anser=JOptionPane.showInputDialog("Treasure waits beyond the reach gold within a hold, remnants of the dead are stitched to keep the living whole Broken bones among the weeds scattered 'round and round, where untouched gems wait silently o'er blades upon the ground. ");
+if (anser.contentEquals("this is a bird nest")) {
+	JOptionPane.showMessageDialog(null, "correct");
+	score++;
+}
+else {
+	JOptionPane.showMessageDialog(null, "wrong");	
+}
 
-		// 2. Make a pop up to show the score.
-																														
+// 2. Make a pop up to show the score.
+	JOptionPane.showMessageDialog(null, score);																													
 	}
 }
 
--- src/_09_zombie_eyes/zombie_eyes/zombie_eyes.pde
+++ src/_09_zombie_eyes/zombie_eyes/zombie_eyes.pde
@@ -1,6 +1,7 @@
-
+PImage cat;
 void setup() {
-
+  cat = loadImage("z.c cat");
+  size(400,400);
 }
 void draw() {
 
