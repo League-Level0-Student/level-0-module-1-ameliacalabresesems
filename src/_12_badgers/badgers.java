28d96608663d59774c97347ebe5478546ff004f1
calabrese.amelia <calabrese.amelia@SEMS-259-36.SEMS259.LAB>
2020-02-12 14:53:32 -0800

    Banana split

--- src/_12_badgers/badgers.java
+++ src/_12_badgers/badgers.java
@@ -1,5 +1,27 @@
 package _12_badgers;
 
 public class badgers {
+	public static void main(String[] args) {
 
+//	System.out.println("Badger, badger, badger, Badger, badger, badger, Badger, badger, badger, Badger, badger, badger, Mushroom, mushroom, A badger, badger, Badger, badger, badger, Badger, badger, badger, Badger, badger, badger, Mushroom, mushroom, A badger, badger, Badger, badger, badger, Badger, badger, badger, Badger, badger, badger, Mush-mushroom, A badger, badger, Badger, badger, badger, Badger, badger, badger, Badger, badger, badger, A snake, a snakeSnaaake! A snaaaake, Oooh, it's a snake, It's a badger, badger, badger, Badger, badger, badger, Badger, badger, badger, Badger, badger, badger, Mushroom, mushroom, Badger, badger, badger, Badger, badger, badger, Badger, badger, badger, Badger, badger, badger, Mushroom, mushroom, A badger, badger, Badger, badger, badger, Badger, badger, badger, Badger, badger, badger, Mushroom, mushroom, A badger, badger, Badger, badger, badger, Badger, badger, badger, Badger, badger, badgerMush-mushroom, A badger, badger, Badger, badger, badger, ,Badger, badger, badger, Badger, badger, badger");	
+//(Mistake I didn't want to delete, 'cause it took so long)
+
+		for (int q = 0; q < 4; q++) {
+
+			for (int i = 0; i < 4; i++) {
+				for (int z = 0; z < 3; z++) {
+					System.out.print("badger, ");
+				}
+				System.out.println("");
+
+			}
+			for (int a = 0; a < 2; a++) {
+				System.out.print("mushroom, ");
+
+			}
+			System.out.println("");
+		
+		}
+		System.out.println("A snake, a snake, Snaaake! A snaaaake,Oooh, it's a snake");
+	}
 }
