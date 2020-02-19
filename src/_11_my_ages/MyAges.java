ec725f403ba332882b9dec79dc3154f881015655
calabrese.amelia <calabrese.amelia@SEMS-259-36.SEMS259.LAB>
2020-02-19 14:54:51 -0800

    checkpoint

--- src/_11_my_ages/MyAges.java
+++ src/_11_my_ages/MyAges.java
@@ -1,6 +1,40 @@
 package _11_my_ages;
 
 public class MyAges {
+	
+	
+	
+	
+	
+	
+	
+	
+	
+       	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
 
 	public static void main(String[] args) {
 		for (int i = 0; i <=11 ; i++) {
---
+++ src/_14_banana_split/banana_split/banana_split.pde
@@ -0,0 +1,11 @@
+    void setup(){
+      for (int i = 0; i < 3 ; i++) {
+      text("ice cream", 25, 15*i+25);
+      }  
+      text("banana", 25, 70);
+    }
+
+    void draw(){
+
+
+    }
